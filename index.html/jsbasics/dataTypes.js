daded36898954f2332d9bd77fee84455d57127f5
unknown <laxmi.katwalsp25@cps.edu.np>
2025-11-06 18:05:17 -0800

    feast: js basics done

---
+++ index.html/jsbasics/dataTypes.js
@@ -0,0 +1,13 @@
+// let nameofCollege ="techspire";
+// const age = 25;
+// const yourAgeGreaterThan21 = true;
+// nameOfCollege="moktan"
+
+// const home["budanilkantha", "baneshwor","putakisadak",10,true,null,undefined];
+
+
+const animal = "dog";
+
+{
+    console.log(animal);
+}
